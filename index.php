ad11ab878d396cb8baa6ff08867df43be8a4e655
Roger Espinosa <roger@umich.edu>
2017-03-02 16:43:09 -0500

    something

--- index.php
+++ index.php
@@ -8,10 +8,6 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/views',
 ));
 
-// $app['upload.controller'] = $app->share(function() use ($app) {
-//     return new DLXS\Controller\UploadController();
-// });
-
 $app->get('/info', function(Silex\Application $app)  {
     $output = "<p>Hello, world</p>";
     $data = $app['db']->fetchAssoc("SELECT * FROM bc_test");
@@ -26,14 +22,6 @@ $app->get('/camera', function() use ($app) {
 });
 
 $app->post('/upload', 'App\Controller\UploadController::saveAction');
-
-// $app->post('/upload', function(Request $request) use ($app) {
-//     $data_js = $request->get('imgBase64');
-//     $data_to_php = explode(',', $data_js);
-//     $data = base64_decode($data_to_php[1]);
-//     file_put_contents(__DIR__ . '/files/test.png', $data);
-//     return "OK";
-// });
 
 $app['debug'] = true;
 
